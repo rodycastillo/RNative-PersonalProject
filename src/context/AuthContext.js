c0123dbc1af4c5062280794612a703b769ca7cc2
Carlito <rody.castillo.tacuri@gmail.com>
2022-05-02 17:45:57 -0500

    feat[AuthContext]: Add create, logout & login user methods

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,6 @@
 // import React, {createContext, useReducer, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from "../api/api"
 import React, {createContext, useReducer} from 'react';
 import {authReducer} from './AuthReducer';
 
@@ -13,8 +15,37 @@ export const AuthContext = createContext({});
 export const AuthProvider = ({children}) => {
   const [state, dispatch] = useReducer(authReducer, authInitialState);
 
+    const signIn = async (data) => {
+      try {
+        const {user, token} = await api.post('/login', data)
+        dispatch({
+          type: "LOGIN",
+          payload: {
+            token,
+            user
+          }
+        })
+        await AsyncStorage.setItem('token', data.token)
+      } catch (error) {
+        console.log('Error: ' +  error)
+      }
+    }
+    const signUp = async (data) => {
+      try {
+        await api.post('register', data)
+      } catch (error) {
+        console.log('Error: ' +  error)
+      }
+    }
+
+    const logOut =  async() => {
+      await AsyncStorage.removeItem('token')
+      dispatch({ type: 'LOGOUT'})
+    }
+
+
   return (
-    <AuthContext.Provider value={{state, dispatch}}>
+    <AuthContext.Provider value={{...state, signIn, signUp, logOut}}>
       {children}
     </AuthContext.Provider>
   );
--- src/context/AuthReducer.js
+++ src/context/AuthReducer.js
@@ -2,12 +2,14 @@ export const authReducer = (state, action) => {
   switch (action.type) {
     case 'LOGIN':
       return {
+        ...state,
         user: action.payload.user,
         status: 'authenticated',
         token: action.payload.token,
       };
     case 'LOGOUT':
       return {
+        ...state,
         user: null,
         status: 'not-authenticated',
         token: null,
