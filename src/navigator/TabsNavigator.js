742147a8df6bd7629a1354f9277df0d9495576be
Carlito <rody.castillo.tacuri@gmail.com>
2022-05-03 23:15:44 -0500

    feat[tabs]: add tabs navigator

---
+++ src/navigator/TabsNavigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export const TabsNavigator = () => {
+  return (
+    <Tab.Navigator>
+      {/* <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+    </Tab.Navigator>
+  );
+}
