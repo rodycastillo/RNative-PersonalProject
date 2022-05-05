12374bb96284c1246ec14fe4962f2cb5d8dbb30c
Carlito <rody.castillo.tacuri@gmail.com>
2022-05-04 23:45:10 -0500

    feat[screens]: add new views

--- src/navigator/TabsNavigator.js
+++ src/navigator/TabsNavigator.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from "../screens/HomeScreen"
+import ProfileScreen from "../screens/ProfileScreen"
 
 const Tab = createBottomTabNavigator();
 
 export const TabsNavigator = () => {
   return (
     <Tab.Navigator>
-      {/* <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+      <Tab.Screen name="Home" component={ HomeScreen } />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
---
+++ src/screens/NewCiteScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const NewCite = () => {
+  return (
+    <View>
+      <Text>New Cite Screen</Text>
+    </View>
+  );
+};
+
+export default NewCite;
